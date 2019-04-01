d2b493124dedd3e7e76dd29a29fe40ec64c2cd5c
Arthur Monney <monneylobe@gmail.com>
2019-04-01 20:24:12 +0100

    update dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -3,13 +3,13 @@
 
 @section('content')
 
-    <div class="wrapper-md">
+    <div class="wrapper-md bg-white">
 
         <div class="row">
-            <div class="col-md-7">
+            <div class="col-sm-12 col-md-6">
                 <div class="card card-welcome">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ __('Welcome') }}</h5>
+                    <div>
+                        <h5 class="card-title">{{ __('Dashboard') }}</h5>
                         <div class="cart-content">
                             <div class="card-logo">
                                 <img src="{{ asset('/shopper/img/shopper.svg') }}" alt="Shopper logo">
@@ -22,6 +22,12 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="col-sm-12 col-md-3">
+
+            </div>
+            <div class="col-sm-12 col-md-3">
+
             </div>
         </div>
 
