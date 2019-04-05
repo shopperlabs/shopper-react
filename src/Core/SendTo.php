c760a50a3cf9af2a8f54e00220cbcc80aeea49fb
Arthur Monney <monneylobe@gmail.com>
2019-04-05 16:56:55 +0100

    Add Shopper Facebook and Twitter API ton send message

--- src/Core/SendTo.php
+++ src/Core/SendTo.php
@@ -2,15 +2,19 @@
 
 namespace Mckenziearts\Shopper\Core;
 
+use Mckenziearts\Shopper\Core\Twitter\Api AS TwitterApi;
+use Mckenziearts\Shopper\Core\Facebook\Api AS FacebookApi;
+
 class SendTo
 {
     /**
      * Send message to Twitter
      *
      * @param $message
-     * @param null $media
+     * @param array $media
      * @param array $options
      * @return Twitter\stdClass
+     * @throws Twitter\TwitterException
      */
     public static function Twitter($message, $media = [], $options = [])
     {
@@ -23,13 +27,14 @@ class SendTo
      * @param $type
      * @param $data
      * @return bool
+     * @throws \Exception
      */
     public static function Facebook($type, $data)
     {
         switch ($type) {
             case 'link':
                 $message = isset($data['message']) ? $data['message'] : '';
-                $result = FacebookApi::sendLink($data['link'], $data['message']);
+                $result = FacebookApi::sendLink($data['link'], $message);
                 break;
             case 'photo':
                 $message = isset($data['message']) ? $data['message'] : '';
